fcf45460a33c19b734d6b0d47dea4ace5813ffe9
东埔灵 <kangyangyi@hotmail.com>
2025-12-27 16:21:51 +0800

    Add files via upload

--- ws02_tools/src/cpp03_tf_broadcaster/src/demo03_point_publisher.cpp
+++ ws02_tools/src/cpp03_tf_broadcaster/src/demo03_point_publisher.cpp
@@ -1,55 +1,56 @@
-/* ${workspaceFolder}/**
-/opt/ros/humble/include/**
-/opt/ros/humble/include/std_msgs/**
-/opt/ros/humble/include/rclcpp/**
-/opt/ros/humble/include/rcl/**
-/opt/ros/humble/include/rclcpp_action/**
-/opt/ros/humble/include/rclcpp
-/opt/ros/humble/include/rcl
-/opt/ros/humble/include/std_msgs
-/opt/ros/humble/include/rcutils
-/opt/ros/humble/include/rmw
-/opt/ros/humble/include/rosbag2_cpp
-${workspaceFolder}/chapt1/ws01/build/interface_demo/rosidl_generator_cpp
-/opt/ros/humble/include/rosidl_runtime_cpp
-/opt/ros/humble/include/geometry_msgs
-/opt/ros/humble/include/turtlesim
-/opt/ros/humble/include/rosidl_runtime_c
-/opt/ros/humble/include/builtin_interfaces */
+/*  
+    需求：发布雷达坐标系中某个坐标点相对于雷达（laser）坐标系的位姿。
+    步骤：
+        1.包含头文件；
+        2.初始化 ROS 客户端；
+        3.定义节点类；
+            3-1.创建坐标点发布方；
+            3-2.创建定时器；
+            3-3.组织并发布坐标点消息。
+        4.调用 spin 函数，并传入对象指针；
+        5.释放资源。
+
+
+*/
+// 1.包含头文件；
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/point_stamped.hpp"
+
 using namespace std::chrono_literals;
-class MinimalPointPublisher : public rclcpp::Node{
-    private:
-        rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr point_pub_;
-        rclcpp::TimerBase::SharedPtr timer_;
-        double_t x;
-    public:
-        MinimalPointPublisher():Node("minimal_point_publisher"), x(0.1){
-            point_pub_ = this->create_publisher<geometry_msgs::msg::PointStamped>("point", 10);
-            timer_ = this->create_wall_timer(100ms, std::bind(&MinimalPointPublisher::on_timer, this));
-        }
-    private:
-        void on_timer(){
-            geometry_msgs::msg::PointStamped point;
-            point.header.frame_id = "laser";
-            point.header.stamp = this->now();
-            x += 0.04;
-            point.point.x = x;
-            point.point.y = 0.0;
-            point.point.z = 0.1;
-            point_pub_->publish(point);
-            RCLCPP_INFO(
-                this->get_logger(), 
-                "发布点: (%.2f, %.2f, %.2f) 坐标系: %s",
-                point.point.x, point.point.y, point.point.z,
-                point.header.frame_id.c_str()
-            );
-        }
+
+// 3.定义节点类；
+class MinimalPointPublisher: public rclcpp::Node {
+public:
+    MinimalPointPublisher(): Node("minimal_point_publisher"),x(0.1){
+        // 3-1.创建坐标点发布方；
+        point_pub_ = this->create_publisher<geometry_msgs::msg::PointStamped>("point",10);
+        // 3-2.创建定时器；
+        timer_ = this->create_wall_timer(0.1s,std::bind(&MinimalPointPublisher::on_timer, this));
+    }
+private:
+    void on_timer(){
+        // 3-3.组织并发布坐标点消息。
+        geometry_msgs::msg::PointStamped point;
+        point.header.frame_id = "laser";
+        point.header.stamp = this->now();
+        x += 0.004;
+        point.point.x = x;
+        point.point.y = 0.0;
+        point.point.z = 0.1;        
+        point_pub_->publish(point);
+    }
+    rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr point_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    double_t x;
 };
-int main(int argc, char** argv){
-    rclcpp::init(argc, argv);
+
+int main(int argc, char const *argv[])
+{
+    // 2.初始化 ROS 客户端；
+    rclcpp::init(argc,argv);
+    // 4.调用 spin 函数，并传入对象指针；
     rclcpp::spin(std::make_shared<MinimalPointPublisher>());
+    // 5.释放资源。
     rclcpp::shutdown();
     return 0;
 }
--- ws02_tools/src/cpp04_tf_lisenter/src/demo01_tf_listener.cpp
+++ ws02_tools/src/cpp04_tf_lisenter/src/demo01_tf_listener.cpp
@@ -5,66 +5,62 @@
     1.包含头文件；
     2.初始化 ROS 客户端；
     3.定义节点类；
-      3-1.创建tf缓存对象指针,融合多个坐标系关系为一颗坐标树；
-      3-2.创建tf监听器，将所有广播的数据写入缓存；
-      3-3.按照条件查找符合条件的坐标系并生成变换后的坐标帧，编写定时器，循环实现转换
+      3-1.创建tf缓存对象指针；
+      3-2.创建tf监听器；
+      3-3.按照条件查找符合条件的坐标系并生成变换后的坐标帧。
     4.调用 spin 函数，并传入对象指针；
     5.释放资源。
 
 */
-/*
-
-*/
-#include <rclcpp/rclcpp.hpp>
+#include "rclcpp/rclcpp.hpp"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 #include "tf2/LinearMath/Quaternion.h"
+
 using namespace std::chrono_literals;
 
-class TFLisenter : public rclcpp::Node {
-    public:
-        TFLisenter():Node("tf_lisenter_node_cpp"){
-            //3.1创建缓存对象
-            buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock()); 
-            //3.2创建监听器对象
-            lisenter_ = std::make_unique<tf2_ros::TransformListener>(*buffer_, this);
-            //3.3编写定时器 
-            timer_ = this->create_wall_timer(100ms, std::bind(&TFLisenter::on_timer, this));
-        }
-    private:
-        void on_timer(){
-            //实现坐标系转换
-            try{
-            //目标坐标系  源坐标系  （雷达能检测到障碍物相对于自身的位置）
-            //TimePointZero:获取最新可用的坐标变换
-            //geometry_msgs::msg::TransformStamped 返回新的坐标帧
-            //lookupTransform(const std::string &target_frame,  //新坐标帧的父坐标系
-            //const std::string &source_frame,  //新坐标帧的子坐标系
-            //const tf2::TimePoint &time)  //转换的时间点，tf2::TimePointZero转换最新时刻的坐标帧
-            //当转换失败时会抛出异常，用try catch处理
-                auto transformStamped = buffer_->lookupTransform("camera", "laser", tf2::TimePointZero);
-                RCLCPP_INFO(this->get_logger(),"----------------------转换结果----------------------");
-                RCLCPP_INFO(this->get_logger(),"frame_id:%s", transformStamped.header.frame_id.c_str());
-                RCLCPP_INFO(this->get_logger(),"child_frame_id:%s",transformStamped.child_frame_id.c_str());
-                RCLCPP_INFO(this->get_logger(),"坐标:(%.2f,%.2f,%.2f)",
-                    transformStamped.transform.translation.x,
-                    transformStamped.transform.translation.y,
-                    transformStamped.transform.translation.z);
-            }
-            catch (const tf2::LookupException& e){
-                RCLCPP_INFO(this->get_logger(), "异常，%s", e.what());
-            }
-        }
-        std::unique_ptr<tf2_ros::Buffer> buffer_;
-        std::shared_ptr<tf2_ros::TransformListener> lisenter_;
-        rclcpp::TimerBase::SharedPtr timer_;
+// 3.定义节点类；
+class MinimalFrameListener : public rclcpp::Node {
+public:
+  MinimalFrameListener():Node("minimal_frame_listener"){
+    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    timer_ = this->create_wall_timer(1s, std::bind(&MinimalFrameListener::on_timer,this));
+  }
+
+private:
+  void on_timer(){
+    try
+    {
+      auto transformStamped = tf_buffer_->lookupTransform("camera","laser",tf2::TimePointZero);
+      RCLCPP_INFO(this->get_logger(),"----------------------转换结果----------------------");
+      RCLCPP_INFO(this->get_logger(),"frame_id:%s",transformStamped.header.frame_id.c_str());
+      RCLCPP_INFO(this->get_logger(),"child_frame_id:%s",transformStamped.child_frame_id.c_str());
+      RCLCPP_INFO(this->get_logger(),"坐标:(%.2f,%.2f,%.2f)",
+                transformStamped.transform.translation.x,
+                transformStamped.transform.translation.y,
+                transformStamped.transform.translation.z);
+
+    }
+    catch(const tf2::LookupException& e)
+    {
+      RCLCPP_INFO(this->get_logger(),"坐标变换异常：%s",e.what());
+    }
+
+
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::shared_ptr<tf2_ros::TransformListener> transform_listener_;
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 };
 
-int main(int argc, char** argv){
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<TFLisenter>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char const *argv[])
+{
+  // 2.初始化 ROS 客户端；
+  rclcpp::init(argc,argv);
+  // 4.调用 spin 函数，并传入对象指针；
+  rclcpp::spin(std::make_shared<MinimalFrameListener>());
+  // 5.释放资源。
+  rclcpp::shutdown();
+  return 0;
 }
-
